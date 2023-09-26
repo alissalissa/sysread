48acf4213feb3e4c8b3d8f8fa0141a0f9f91cbb8
Alissa <alissa@murena.io>
2023-09-26 17:12:46 -0400

    Implement lvnr
    Implemented long variable name record struct, and associated factories / destructors.

---
+++ include/lvnr.h
@@ -0,0 +1,29 @@
+//Long variable names record
+
+#ifndef __SYSREAD_LVNR_H__
+#define __SYSREAD_LVNR_H__
+
+#define LVNR_SIZE_CHECK 1
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bstream.h"
+
+typedef struct {
+	int32_t rec_type; //Should be always 7
+	int32_t subtype; //Should be always 13
+	int32_t count; //Keys and values should always be of length count
+	bstream_t *keys; //At most 8 bits wide
+	bstream_t *values; //At most 64 bits wide
+	bool constructed;
+}lvnr_t;
+
+lvnr_t *lvnr_new(int32_t,int32_t,int32_t,bstream_t*,bstream_t*);
+lvnr_t *lvnr_fnew(FILE*);
+bool lvnr_destroy(lvnr_t*);
+
+#endif
---
+++ src/lvnr.c
@@ -0,0 +1,114 @@
+//Implementation for long variable name records
+#include "lvnr.h"
+
+lvnr_t *lvnr_new(int32_t rec_type,int32_t subtype,int32_t count,bstream_t *keys,bstream_t *values){
+	if(rec_type!=7 || subtype!=13)
+		return NULL;
+	lvnr_t *ret=(lvnr_t*)malloc(sizeof(lvnr_t));
+	if(!ret)
+		return NULL;
+
+	ret->rec_type=rec_type;
+	ret->subtype=subtype;
+	ret->count=count;
+	
+	if(ret->count>0){
+		ret->keys=bstream_cnew(keys);
+		ret->values=bstream_cnew(values);
+		if(!ret->keys || !ret->values){
+			if(ret->keys)
+				bstream_destroy(ret->keys);
+			if(ret->values)
+				bstream_destroy(ret->values);
+			free(ret);
+			return NULL;
+		}
+	}else{
+		ret->keys=NULL;
+		ret->values=NULL;
+	}
+	ret->constructed=true;
+	return ret;
+}
+
+lvnr_t *lvnr_fnew(FILE *handle){
+	assert(handle);
+	
+	int32_t rec_type=0;
+	fread(&rec_type,sizeof(int32_t),1,handle);
+	if(rec_type!=7 || feof(handle) || ferror(handle)){
+		printf("Long variable name record corrupted in file...\n");
+		return NULL;
+	}
+
+	int32_t subtype=0;
+	fread(&subtype,sizeof(int32_t),1,handle);
+	if(subtype!=13 || feof(handle) || ferror(handle)){
+		printf("Long variable name record corrupted in file...\n");
+		return NULL;
+	}
+
+	int32_t n=0;
+	fread(&n,sizeof(int32_t),1,handle);
+	if(ferror(handle)){
+		printf("Long variable name record corrupted in file...\n");
+		return NULL;
+	}
+
+	bstream_t *stream=bstream_new();
+	if(!stream)
+		return NULL;
+	stream->length=n;
+	stream->stream=(char*)calloc(n,sizeof(char));
+	if(!stream->stream){
+		free(stream);
+		return NULL;
+	}
+	bstream_t *keys=(n>0)?(bstream_t*)calloc(n,sizeof(bstream_t)):NULL;
+	bstream_t *values=(n>0)?(bstream_t*)calloc(n,sizeof(bstream_t)):NULL;
+	int32_t n_of_pairs=0;
+	if(n>0){
+		fread(stream->stream,sizeof(char),n,handle);
+		int32_t n_of_pairs=bstream_count(*stream,0x09);
+		bstream_t **pairs=bstream_split(*stream,0x09);
+		for(int i=0;i<n_of_pairs;i++){
+			bstream_t **pair=bstream_split(*pairs[i],'=');
+			keys[i]=*pair[0];
+			values[i]=*pair[1];
+			bstream_destroy(pair[0]);
+			bstream_destroy(pair[1]);
+			free(pair);
+		}
+		for(int i=0;i<n_of_pairs;i++){
+			bstream_destroy(pairs[i]);
+			free(pairs);
+		}
+	}
+	lvnr_t *ret=lvnr_new(rec_type,subtype,n_of_pairs,keys,values);
+	for(int i=0;i<n_of_pairs;i++){
+		free(keys[i].stream);
+		free(values[i].stream);
+	}
+	free(keys);
+	free(values);
+	bstream_destroy(stream);
+	return ret;
+}
+
+bool lvnr_destroy(lvnr_t *haystack){
+	if(!haystack || !haystack->keys || !haystack->values || !haystack->constructed)
+		return false;
+	haystack->constructed=false;
+	for(int i=0;i<haystack->count;i++){
+		if(haystack->keys[i].stream){
+			free(haystack->keys[i].stream);
+			haystack->keys[i].length=-1;
+		}
+		if(haystack->values[i].stream){
+			free(haystack->values[i].stream);
+			haystack->values[i].length=-1;
+		}
+	}
+	haystack->count=-1;
+	return true;
+}
