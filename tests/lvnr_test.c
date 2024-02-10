a7c08dcf3000bce55ec913ef0b0a5ba545d60f61
Alissa <alissa@murena.io>
2024-02-10 13:19:24 -0500

    Switching toolchain and beginning to implement libcheck

--- tests/lvnr_test.c
+++ tests/lvnr_test.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
+#include <check.h>
+
 #include "lvnr.h"
 #include "writer.h"
 
-int main(int argc,char *argv[]){
+START_TEST(lvnr_parse){
 	char path[]="./lvnr.sys";
-	printf("Testing lvnr.....\n");
-	if(!write_lvnr(path)){
-		printf("Error writing test system file...\n");
-		return 1;
-	}
+	ck_assert(write_lvnr(path));
 	FILE *handle=fopen(path,"rb");
-	if(!handle){
-		printf("Unable to open system handle for reading....");
-		return 1;
-	}
+	ck_assert(handle);
 	lvnr_t *foo=lvnr_fnew(handle);
 	fclose(handle);
-	printf("%s\n",(foo)?"Success!":"Failed :-(");
+	ck_assert(foo);
 	if(foo)
-		lvnr_destroy(foo);
-	return 0;
+		ck_assert(lvnr_destroy(foo));
+}
+END_TEST
+
+int main(int argc,char *argv[]){
+	Suite *s=suite_create("lvnr");
+	TCase *tc_core=tcase_create("core");
+	tcase_add_test(tc_core,lvnr_parse);
+	suite_add_tcase(s,tc_core);
+	SRunner *sr=srunner_create(s);
+	srunner_run_all(sr,CK_NORMAL);
+	int number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
